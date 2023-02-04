b91bf8dcb28abd13fb5f256315383a6b959f6e43
Allen Gu <allengu01@berkeley.edu>
2023-02-04 11:39:21 -0800

    try supersampling without using sample buffer

--- src/rasterizer.cpp
+++ src/rasterizer.cpp
@@ -89,23 +89,49 @@ namespace CGL {
     float x2, float y2,
     Color color) {
     // TODO: Task 1: Implement basic triangle rasterization here, no supersampling
-    float min_x = floor(min(x0, min(x1, x2))) + 0.5;
-    float max_x = ceil(max(x0, max(x1, x2))) - 0.5;
-    float min_y = floor(min(y0, min(y1, y2))) + 0.5;
-    float max_y = ceil(max(y0, max(y1, y2))) - 0.5;
+//    float min_x = floor(min(x0, min(x1, x2))) + 0.5;
+//    float max_x = ceil(max(x0, max(x1, x2))) - 0.5;
+//    float min_y = floor(min(y0, min(y1, y2))) + 0.5;
+//    float max_y = ceil(max(y0, max(y1, y2))) - 0.5;
+//
+//    for (float x = min_x; x <= max_x; x++) {
+//      for (float y = min_y; y <= max_y; y++) {
+//        if (inside_triangle(x0, y0, x1, y1, x2, y2, x, y)) {
+//          rasterize_point(x, y, color);
+//        }
+//      }
+//    }
 
-    for (float x = min_x; x <= max_x; x++) {
-      for (float y = min_y; y <= max_y; y++) {
-        if (inside_triangle(x0, y0, x1, y1, x2, y2, x, y)) {
-          rasterize_point(x, y, color);
+    // TODO: Task 2: Update to implement super-sampled rasterization
+    double step = 1.0 / (sqrt(this->sample_rate));
+    int min_x = floor(min(x0, min(x1, x2))) - 1;
+    int max_x = ceil(max(x0, max(x1, x2))) + 1;
+    int min_y = floor(min(y0, min(y1, y2))) - 1;
+    int max_y = ceil(max(y0, max(y1, y2))) + 1;
+
+    int iters = round(sqrt(this->sample_rate));
+    for (int i = min_x; i <= max_x; i++) {
+      for (int j = min_y; j <= max_y; j++) {
+        Color c = Color(0, 0, 0);
+        bool inside = false;
+        for (int x = 0; x < iters; x++) {
+          for (int y = 0; y < iters; y++) {
+            int u = i + (x * step) + (step / 2);
+            int v = j + (y * step) + (step / 2);
+            if (inside_triangle(x0, y0, x1, y1, x2, y2, u, v)) {
+              inside = true;
+              c += color * (1.0 / this->sample_rate);
+            } else {
+              c += Color::White * (1.0 / this->sample_rate);
+            }
+          }
+        }
+        double center_x = i, center_y = j;
+        if (inside) {
+          rasterize_point(center_x, center_y, c);
         }
       }
     }
-
-    // TODO: Task 2: Update to implement super-sampled rasterization
-
-
-
   }
 
 
@@ -137,10 +163,7 @@ namespace CGL {
 
   void RasterizerImp::set_sample_rate(unsigned int rate) {
     // TODO: Task 2: You may want to update this function for supersampling support
-
     this->sample_rate = rate;
-
-
     this->sample_buffer.resize(width * height, Color::White);
   }
 
